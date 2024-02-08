000000004cb04cd7648134186ccb57af0015f825
Kai Ito <kai.ito@gmail.com>
2024-02-08 12:47:25 +0100

    Initial commit

---
+++ Web/Program.fs
@@ -0,0 +1,25 @@
+﻿module Program
+
+open System
+open Microsoft.AspNetCore.Hosting
+
+open Microsoft.AspNetCore.Builder
+
+let configureServices (builder: WebApplicationBuilder) = builder
+
+let app =
+    Environment.GetCommandLineArgs()
+    |> WebApplication.CreateBuilder
+    |> fun builder -> builder.WebHost.UseUrls("https://0.0.0.0:5001")
+    |> configureServices
+    |> fun builder -> builder.Build()
+    |> fun app ->
+        app
+            .UseHttpsRedirection()
+            .UseRouting()
+            .UseStaticFiles()
+        |> ignore
+
+        app
+
+app.Run()

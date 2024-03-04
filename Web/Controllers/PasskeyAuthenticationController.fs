473f87fe0fa79b628df26cb7b5b1c7ce4906250b
Kai Ito <kai.ito@valora.com>
2024-03-04 18:38:33 +0100

    Rename and extract stuff. Use HTMX to do the ajax

--- Web/Controllers/PasskeyAuthenticationController.fs
+++ Web/Controllers/PasskeyAuthenticationController.fs
@@ -10,30 +10,27 @@ open Microsoft.Extensions.Configuration
 open Web.Models
 open Web.Services
 
-type AuthenticationController(config: IConfiguration, authService: SupabaseAuthService) =
+type PasskeyAuthenticationController(config: IConfiguration, authService: SupabaseAuthService) =
     inherit Controller()
 
     [<HttpGet>]
-    member this.SignIn() = this.View()
-
-    [<HttpGet>]
-    member this.PasswordlessSignIn() =
+    member this.SignIn() =
         let model = {
-            PasswordlessSignUpModel.PublicKey = config["Passwordless:PublicKey"].ToString()
+            PasskeyModel.PublicKey = config["Passwordless:PublicKey"].ToString()
         }
 
         this.View(model)
 
     [<HttpGet>]
-    member this.PasswordlessSignUp() =
+    member this.SignUp() =
         let model = {
-            PasswordlessSignUpModel.PublicKey = config["Passwordless:PublicKey"].ToString()
+            PasskeyModel.PublicKey = config["Passwordless:PublicKey"].ToString()
         }
 
         this.View(model)
 
     [<HttpGet>]
-    member this.PasswordlessSignInWebAuthn([<FromQuery>] token: string) =
+    member this.WebAuthnSignIn([<FromQuery>] token: string) =
         task {
             let content = JsonSerializer.Serialize({| token = token |})
             let content = new StringContent(content, Encoding.UTF8, "application/json")
@@ -57,16 +54,15 @@ type AuthenticationController(config: IConfiguration, authService: SupabaseAuthS
 
                 return this.RedirectToAction("Index", "Home")
             else
-                return this.RedirectToAction("PasswordlessSignIn", "Authentication")
+                return this.RedirectToAction("SignIn")
         }
 
     [<HttpPost>]
-    member this.PasswordlessSignUpWebAuthn([<FromForm>] username: string) : Task<JsonResult> =
+    member this.WebAuthnSignUp([<FromForm>] username: string) : Task<JsonResult> =
         task {
             let payload = {|
                 userId = Guid.NewGuid()
                 username = username
-                aliases = [| username |]
             |}
 
             use client = new HttpClient()
@@ -91,43 +87,6 @@ type AuthenticationController(config: IConfiguration, authService: SupabaseAuthS
                     .token
 
             return JsonResult(token)
-        }
-
-    [<HttpPost>]
-    [<ValidateAntiForgeryToken>]
-    member this.SignIn([<FromForm>] signIn: SignUpPostModel) =
-        task {
-            do! authService.SignIn(signIn.Email, signIn.Password)
-
-            return this.RedirectToAction("Index", "Home")
-        }
-
-    [<HttpGet>]
-    member this.SignUp() = this.View()
-
-    [<HttpPost>]
-    [<ValidateAntiForgeryToken>]
-    member this.SignUp([<FromForm>] signUp: SignUpPostModel) =
-        task {
-            do! authService.SignUp(signUp.Email, signUp.Username, signUp.Password)
-            return this.RedirectToAction(nameof this.ConfirmEmail)
-        }
-
-    [<HttpGet>]
-    member this.ConfirmEmail() = this.View()
-
-    [<HttpGet>]
-    member this.Confirmed() =
-        let queryParams = base.Request.Query
-
-        task {
-            if queryParams |> Seq.isEmpty |> not then
-                let accessToken = queryParams["access_token"]
-                let refreshToken = queryParams["refresh_token"]
-
-                do! authService.SignInWithTokens(accessToken, refreshToken)
-
-            return this.View()
         }
 
     [<HttpGet>]
---
+++ Web/Controllers/SupabaseAuthenticationController.fs
@@ -0,0 +1,58 @@
+namespace Web.Controllers
+
+open System
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Configuration
+open Web.Models
+open Web.Services
+
+type SupabaseAuthenticationController(config: IConfiguration, authService: AuthService) =
+    inherit Controller()
+
+    [<HttpGet>]
+    member this.SignIn() = this.View()
+
+    [<HttpPost>]
+    [<ValidateAntiForgeryToken>]
+    member this.SignIn([<FromForm>] signIn: SignUpPostModel) =
+        task {
+            do! authService.SignIn(signIn.Email, signIn.Password)
+
+            return this.RedirectToAction("Index", "Home")
+        }
+
+    [<HttpGet>]
+    member this.SignUp() = this.View()
+
+    [<HttpPost>]
+    [<ValidateAntiForgeryToken>]
+    member this.SignUp([<FromForm>] signUp: SignUpPostModel) =
+        task {
+            do! authService.SignUp(signUp.Email, signUp.Username, signUp.Password)
+            return this.RedirectToAction(nameof this.ConfirmEmail)
+        }
+
+    [<HttpGet>]
+    member this.ConfirmEmail() = this.View()
+
+    [<HttpGet>]
+    member this.Confirmed() =
+        let queryParams = base.Request.Query
+
+        task {
+            if queryParams |> Seq.isEmpty |> not then
+                let accessToken = queryParams["access_token"]
+                let refreshToken = queryParams["refresh_token"]
+
+                do! authService.SignInWithTokens(accessToken, refreshToken)
+
+            return this.View()
+        }
+
+    [<HttpGet>]
+    member this.Logout() =
+        task {
+            do! authService.SignOut()
+
+            return this.RedirectToAction("Index", "Home")
+        }
